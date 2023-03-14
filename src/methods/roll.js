9aece8b7b6f143c711f3cbf83edc34ec2fb0bb6e
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-03-14 15:19:33 -0400

    message words array printing data, spaceNormalized message working

--- src/methods/roll.js
+++ src/methods/roll.js
@@ -1,7 +1,29 @@
 const { evaluate } = require("mathjs");
 
 const diceRoller = ({ userInput, settings }) => {
-  return userInput;
+  // if input then remove all spaces, then add spaces around math operators
+  var spaceNormalizer = null;
+  if (userInput) {
+    spaceNormalizer = userInput.replaceAll(" ", "").toLowerCase();
+    var separators = ["+", "-", "*", "/"];
+
+    for (var i = 0; i < separators.length; i++) {
+      var rg = new RegExp("\\" + separators[i], "g");
+      spaceNormalizer = spaceNormalizer.replace(rg, " " + separators[i] + " ");
+    }
+  }
+
+  const message = spaceNormalizer;
+  var messageWords = [];
+
+  try {
+    var array = message.split(" ");
+    array.map((word) => {
+      messageWords.push(word);
+    });
+  } catch {}
+
+  return {messageWords, message};
 };
 
 module.exports = diceRoller;

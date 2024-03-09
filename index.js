3484bcab29b16b8ac09147cc4579afbc15f6d391
Mateo Wallace <mateo.t.wallace@gmail.com>
2024-03-09 14:59:34 -0500

    added param comment to index

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import Dice from "./models/Dice.js";
 
+/**
+ * Call the roll method of the dice class.
+ * @param {string} userInput - The user input for the dice roll.
+ * @param {Object} diceSettings - The settings for the Dice Class.
+ * @return {Object} The result of the dice roll.
+ */
 const roll = (userInput, diceSettings) => {
   if (diceSettings === undefined) diceSettings = {};
   const type = Object.prototype.toString.call(diceSettings).slice(8, -1);
